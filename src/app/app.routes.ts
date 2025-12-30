c8046351343c8ec5933041c2d8db800b62cbbaa9
LuqmanHakim-DX <misterkim147@gmail.com>
2025-12-30 22:20:56 +0800

    Supabase-login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'chat',
+    loadComponent: () =>
+      import('./pages/chat-component/chat-component').then((com) => com.ChatComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login-component/login-component').then((com) => com.LoginComponent),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/login-component/login-component').then((com) => com.LoginComponent),
+  },
+];
